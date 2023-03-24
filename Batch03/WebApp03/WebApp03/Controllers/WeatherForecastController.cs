f810a358bd95a77c67ea2ed830c435bf8c62c410
yankzsoe <yankzsoe@gmail.com>
2023-03-24 22:48:14 +0700

    Add JWT

--- Batch03/WebApp03/WebApp03/Controllers/WeatherForecastController.cs
+++ Batch03/WebApp03/WebApp03/Controllers/WeatherForecastController.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace WebApp03.Controllers
 {
@@ -9,18 +14,21 @@ namespace WebApp03.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet]
         [SwaggerOperation(Summary = "Get weather forecast", Description = "Get weather")]
+        [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
@@ -30,5 +38,32 @@ namespace WebApp03.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost("token")]
+        public IActionResult GenerateToken()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "username"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var issuer = _configuration.GetSection("Jwt:Issuers").Get<string[]>();
+            var audience = _configuration.GetSection("Jwt:Audiences").Get<string[]>();
+            var inSecond = double.Parse(_configuration["Jwt:Duration"]);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer[0],
+                audience: audience[0],
+                claims: claims,
+                expires: DateTime.UtcNow.AddSeconds(inSecond),
+                signingCredentials: creds);
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
     }
 }
--- Batch03/WebApp03/WebApp03/Program.cs
+++ Batch03/WebApp03/WebApp03/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +17,24 @@ builder.Services.AddSwaggerGen(c => {
     });
 });
 
+// Add JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuerSigningKey = true,
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Secret"])),
+            ValidIssuers = builder.Configuration.GetSection("Jwt:Issuers").Get<string[]>(),
+            ValidAudiences = builder.Configuration.GetSection("Jwt:Audiences").Get<string[]>(),
+        };
+    
+    });
+
 var app = builder.Build();
 
 // Use Swagger
@@ -24,6 +46,9 @@ app.UseSwaggerUI(c => {
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+// Add Authentication
+app.UseAuthentication();
 
 app.UseAuthorization();
 
